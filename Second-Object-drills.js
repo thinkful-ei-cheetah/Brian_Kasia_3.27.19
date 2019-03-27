757ce2ba01e6984fbd629370598ad3ca37f01411
Kasia <kasia@miroartdesign.com>
2019-03-27 16:05:34 -0400

    more

--- Second-Object-drills.js
+++ Second-Object-drills.js
@@ -10,17 +10,40 @@ console.log(loaf.hydration());
 console.log(loaf.flour, loaf.water);
 
 const funnyObj = {
-    foo: 'Brian',
-    bar: 50,
-    fum: 'developer',
-    quux: 'blaabla',
-    spam: ' lalalalaaaaa',
-}
+  foo: 'Brian',
+  bar: 50,
+  fum: 'developer',
+  quux: 'blaabla',
+  spam: ' lalalalaaaaa',
+};
 for( let item in funnyObj){
-    console.log(funnyObj[item]);
+  console.log(funnyObj[item]);
 }
 
 const hobbitM = {
-    meals: ['breakfast','second breakfast','elevenses','lunch','afternoon tea','dinner','supper']
-}
+  meals: ['breakfast','second breakfast','elevenses','lunch','afternoon tea','dinner','supper']
+};
 console.log(hobbitM.meals[3]);
+
+const company = [
+  {name:'kasia', jobTitle: 'student', boss: 'Jim'},
+  {name: ' Brian', jobTitle: 'student', boss: 'Jim'},
+  {name: ' jeff' ,jobTitle: 'instructor', boss: 'Jim'},
+  {name: 'Uzair', jobTitle:'mentor', boss: 'Jim' },
+  {name: 'Jim' ,jobTitle: 'owner'}
+];
+
+
+company.forEach(function(person){
+  console.log(person.name, person.jobTitle);
+    
+});
+
+company.forEach(function(person){
+  if (!person.boss){
+    console.log(`${person.jobTitle} ${person.name} reports to nobody`);
+  } else {
+    console.log(`${person.jobTitle} ${person.name} reports to ${person.boss}.`);
+  }
+});
+
