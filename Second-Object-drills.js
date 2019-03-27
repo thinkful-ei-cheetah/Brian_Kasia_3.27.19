baf8811886a6ce1a2c27c10feec07d4556f0adf0
Brian Sandoval <39751822+iambrianshin@users.noreply.github.com>
2019-03-27 16:01:19 -0400

    problem 4,5

--- Second-Object-drills.js
+++ Second-Object-drills.js
@@ -1,26 +1,72 @@
-'use strict';
+"use strict";
 const loaf = {
   flour: 300,
   water: 210,
-  hydration: function(){
-    return (this.water / this.flour)* 100;
+  hydration: function() {
+    return (this.water / this.flour) * 100;
   }
 };
 console.log(loaf.hydration());
 console.log(loaf.flour, loaf.water);
 
 const funnyObj = {
-    foo: 'Brian',
-    bar: 50,
-    fum: 'developer',
-    quux: 'blaabla',
-    spam: ' lalalalaaaaa',
-}
-for( let item in funnyObj){
-    console.log(funnyObj[item]);
+  foo: "Brian",
+  bar: 50,
+  fum: "developer",
+  quux: "blaabla",
+  spam: " lalalalaaaaa"
+};
+for (let item in funnyObj) {
+  console.log(funnyObj[item]);
 }
 
 const hobbitM = {
-    meals: ['breakfast','second breakfast','elevenses','lunch','afternoon tea','dinner','supper']
-}
+  meals: [
+    "breakfast",
+    "second breakfast",
+    "elevenses",
+    "lunch",
+    "afternoon tea",
+    "dinner",
+    "supper"
+  ]
+};
 console.log(hobbitM.meals[3]);
+
+const obj = [
+  {
+    name: "Kasia",
+    jobTitle: "student",
+    boss: "Jim"
+  },
+  {
+    name: "Brian",
+    jobTitle: "student",
+    boss: "Jordan"
+  },
+  {
+    name: "Jordan",
+    jobTitle: "Program Manager",
+    boss: "Rich"
+  },
+  {
+    name: "Rich",
+    jobTitle: "instructor, CEO"
+  }
+];
+
+obj.forEach(person => {
+  console.log(person.name, person.jobTitle);
+});
+
+obj.forEach(person => {
+  if (!person.boss) {
+    console.log(
+      `"${person.jobTitle} ${person.name} doesn't report to anybody."`
+    );
+  } else {
+    console.log(
+      `"${person.jobTitle} ${person.name} reports to ${person.boss}."`
+    );
+  }
+});
